05b634247d87cadfa7c641a5ee3109e33170cdf1
Boris1310 <novyjpolzovatel@MacBook-Pro-Boris.local>
2021-10-07 15:42:36 +0300

    Added MyLogger

---
+++ MyLogger/Logger.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BorysFedyuchok\MyLogger;
+
+class Logger
+{
+
+    public static function setWarning($message,$log){
+        $file = fopen($log,"a");
+        fwrite($file,"[ " .date("h:i:s d.M.Y") . " ] \n ----Warning---- \n [".$message."] \n \n");
+        fclose($file);
+    }
+
+    public static function setNotice($message,$log){
+        $file = fopen($log,"a");
+        fwrite($file,"[ " .date("h:i:s d.M.Y") . " ] \n ----Notice---- \n [".$message."] \n \n");
+        fclose($file);
+    }
+
+    public static function setError($message,$log){
+        $file = fopen($log,"a");
+        fwrite($file,"[ " .date("h:i:s d.M.Y") . " ] \n ----Error---- \n [".$message."] \n \n");
+        fclose($file);
+    }
+
+}
+
+Logger::setError("Hello","current.log");
