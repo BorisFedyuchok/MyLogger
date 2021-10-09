086211d50377236d0b0bc3956e980a990cb34c39
Boris1310 <novyjpolzovatel@MacBook-Pro-Boris.local>
2021-10-09 23:24:57 +0300

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9b6a998b8c327621d05aa615d50e64d2410e7a0b',
+        'name' => 'boris1310/my-logger',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'boris1310/my-logger' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9b6a998b8c327621d05aa615d50e64d2410e7a0b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
